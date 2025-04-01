e558392a8c48d385e386d037c5bc9eaf2ec23d52
nina031 <messaoudinina55@gmail.com>
2025-04-01 18:00:50 +0200

    add backend and start front

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,1 +1,18 @@
+// Importation du CSS
 import './style.css'
+
+// Sélectionner les éléments du DOM
+const display = document.querySelector('#display div');
+const buttons = document.querySelectorAll('.btn');
+
+// Initialiser l'affichage
+display.textContent = '0';
+
+// Ajouter un écouteur d'événements à chaque bouton
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    console.log('Bouton cliqué:', button.textContent);
+    // Pour tester l'affichage, remplaçons le contenu par la valeur du bouton
+    display.textContent = button.textContent;
+  });
+});
